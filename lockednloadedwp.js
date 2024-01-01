96336bebdece239e770eb75185b0689db219cfd0
Kaobara <dannymahmud1@gmail.com>
2024-01-02 01:01:45 +1100

    first commit

---
+++ lockednloadedwp.js
@@ -0,0 +1,11 @@
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['auto'] = 'Auto';
+  	CONFIG.DND5E.weaponProperties['bulky'] = 'Bulky';
+	CONFIG.DND5E.weaponProperties['burst'] = 'Burst';
+	CONFIG.DND5E.weaponProperties['explosive'] = 'Explosive';
+	CONFIG.DND5E.weaponProperties['reliability'] = 'Reliability';
+	CONFIG.DND5E.weaponProperties['repeating'] = 'Repeating';
+	CONFIG.DND5E.weaponProperties['scatter'] = 'Scatter';
+	CONFIG.DND5E.weaponProperties['special'] = 'Special';
+	
+});
